d97e16e97e6ff94f81f58c74a64b4d0cbcd1f54b
Matthew Weil <matt@weil.dev>
2020-11-08 11:58:44 -0500

    structure creation + some terrain

---
+++ Assets/PlayerAudio.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAudio : MonoBehaviour {
+
+    public AudioClip splashSound;
+    public AudioSource audioS;
+
+
+    private void OnTriggerEnter(Collider other) {
+        if(other.CompareTag("Water")){
+            audioS.PlayOneShot(splashSound);
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
